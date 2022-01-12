b4be64412ae093188da51c62eda5c0ce1af6b72f
Meng-MiamiOH <43998638+Meng-MiamiOH@users.noreply.github.com>
2022-01-11 23:47:56 -0500

    fixed GDP graphic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const MARGIN = { LEFT: 40, RIGHT: 20, TOP: 10, BOTTOM: 65 };
-const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT;
-const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM;
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 };
+const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
 
 const svg = d3
   .select("#chart-area")
@@ -8,107 +8,112 @@ const svg = d3
   .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
   .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM);
 
-let flag = true;
+let time = 0;
 
 const g = svg
   .append("g")
   .attr("transform", `translate(${MARGIN.LEFT * 2}, ${MARGIN.TOP})`);
 
-// X label
-g.append("text")
-  .attr("class", "x axis-label")
-  .attr("x", WIDTH / 2)
-  .attr("y", HEIGHT + 60)
-  .attr("font-size", "16px")
-  .attr("text-anchor", "middle")
-  .text("GDP-per-capita");
+// Scales
+const x = d3.scaleLog().base(10).range([0, WIDTH]).domain([142, 150000]);
+const y = d3.scaleLinear().range([HEIGHT, 0]).domain([0, 90]);
 
-// Y label
-g.append("text")
-  .attr("class", "y axis-label")
-  .attr("x", -(HEIGHT / 2))
-  .attr("y", -60)
-  .attr("font-size", "16px")
+const area = d3
+  .scaleLinear()
+  .range([25 * Math.PI, 1500 * Math.PI])
+  .domain([2000, 1400000000]);
+
+const continentColor = d3.scaleOrdinal(d3.schemePastel1);
+
+// Labels
+const xLabel = g
+  .append("text")
+  .attr("x", WIDTH / 2)
+  .attr("y", HEIGHT + 50)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .text("GDP-per-capita ($)");
+
+const yLabel = g
+  .append("text")
+  .attr("x", -170)
+  .attr("y", -40)
+  .attr("font-size", "20px")
   .attr("text-anchor", "middle")
   .attr("transform", "rotate(-90)")
-  .text("Life Expectancy");
+  .text("Life Expectancy (Years)");
 
-const x = d3.scaleBand().range([0, WIDTH]).paddingInner(0.3).paddingOuter(0.2);
+const timeLabel = g
+  .append("text")
+  .attr("x", WIDTH - 240)
+  .attr("y", HEIGHT - 10)
+  .attr("font-size", "60px")
+  .attr("opacity", "0.4")
+  .attr("text-anchor", "middle")
+  .text("1800"); //initial value
 
-const y = d3.scaleLinear().range([HEIGHT, 0]);
-
-const xAxisGroup = g
-  .append("g")
+// X Axis
+const xAxisCall = d3
+  .axisBottom(x)
+  .tickValues([400, 4000, 40000])
+  .tickFormat(d3.format("$"));
+g.append("g")
   .attr("class", "x axis")
-  .attr("transform", `translate(0, ${HEIGHT})`);
+  .attr("transform", `translate(0, ${HEIGHT})`)
+  .call(xAxisCall);
 
-const yAxisGroup = g.append("g").attr("class", "y axis");
+// Y Axis
+const yAxisCall = d3.axisLeft(y);
+g.append("g").attr("class", "y axis").call(yAxisCall);
 
 d3.json("/data/data.json")
   .then((data) => {
-    data.forEach((d) => {
-      d.countries.forEach((c) => {
-        if (c.income === null) c.income = 0;
-        if (c.life_exp === null) c.life_exp = 0;
-      });
+    // clean data
+    const formattedData = data.map((year) => {
+      return year["countries"]
+        .filter((country) => {
+          const dataExists = country.income && country.life_exp;
+          return dataExists;
+        })
+        .map((country) => {
+          country.income = Number(country.income);
+          country.life_exp = Number(country.life_exp);
+          return country;
+        });
     });
 
-    console.log(data);
+    d3.interval(() => {
+      // at the end of our data, loop back
+      time = time < 214 ? time + 1 : 0;
+      update(formattedData[time]);
+    }, 2000);
 
-    // d3.interval(() => {
-    //   update(data);
-    // }, 2000);
-
-    // update(data);
+    // first run of the visualization
+    update(formattedData[0]);
 
     function update(data) {
-      const t = d3.transition().duration(750);
-
-      x.domain(data.map((d) => d.month));
-      y.domain([0, d3.max(data, (d) => d[value])]);
-
-      const xAxisCall = d3.axisBottom(x);
-
-      xAxisGroup
-        .transition(t)
-        .call(xAxisCall)
-        .selectAll("text")
-        .attr("y", "10")
-        .attr("x", "-5")
-        .attr("text-anchor", "end")
-        .attr("transform", "rotate(-40)");
-
-      const yAxisCall = d3
-        .axisLeft(y)
-        .ticks(5)
-        .tickFormat((d) => d + "m");
-      yAxisGroup.transition(t).call(yAxisCall);
+      const t = d3.transition().duration(100);
 
       // JOIN new data with old elements.
-      const recs = g.selectAll("circle").data(data, d=> d.month);
+      const circles = g.selectAll("circle").data(data, (d) => d.country);
 
       // EXIT old elements not present in new data.
-      recs
-        .exit()
-        .attr("fill", "red")
-        .transition(t)
-        .attr("cy", y(0))
-        .remove();
+      circles.exit().remove();
 
       // ENTER new elements present in new data.
-      recs
+      circles
         .enter()
         .append("circle")
-        .attr("cy", y(0))
-        .attr("r", 5)
-        .attr("fill", "grey")
+        .attr("fill", (d) => continentColor(d.continent))
         // UPDATE old elements present in new data.
-        .merge(recs)
+        .merge(circles)
         .transition(t)
-        .attr("cx", (d) => x(d.month) + (x.bandwidth() / 2))
-        .attr("cy", (d) => y(d[value]))
+        .attr("cx", (d) => x(d.income))
+        .attr("cy", (d) => y(d.life_exp))
+        .attr("r", (d) => Math.sqrt(area(d.population) / Math.PI));
 
-
+      // update the time label
+      timeLabel.text(String(time + 1800));
     }
   })
   .catch((err) => {
